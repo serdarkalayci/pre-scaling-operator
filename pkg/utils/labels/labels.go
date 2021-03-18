3057835da0b68a6a3b23d8e75436c7011cb649e4
Thomas Richter <thomasjohnrichter@gmail.com>
2021-03-18 08:59:22 +0100

    Re-add labels package

---
+++ pkg/utils/labels/labels.go
@@ -0,0 +1,14 @@
+package labels
+
+import (
+	"strconv"
+)
+
+func GetLabelValue(labels map[string]string, optin string) bool {
+	var detect bool
+	if v, found := labels[optin]; found {
+
+		detect, _ = strconv.ParseBool(v)
+	}
+	return detect
+}
