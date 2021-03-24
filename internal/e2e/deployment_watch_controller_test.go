adf32f2dc18ff4e8de25865e559694c7d1c2ee25
Andreas Krivas <andreas131313@gmail.com>
2021-03-24 12:54:14 +0100

    Switch back to default namespace

--- internal/e2e/deployment_watch_controller_test.go
+++ internal/e2e/deployment_watch_controller_test.go
@@ -29,7 +29,7 @@ var _ = Describe("e2e Test for the Deployment Watch Controller", func() {
 
 	var key = types.NamespacedName{
 		Name:      "test",
-		Namespace: "devops-scaling-operator",
+		Namespace: "default",
 	}
 
 	BeforeEach(func() {
